0780bd9ab6aac725bece8015f6ab93228e2139c1
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2022-12-06 12:28:03 -0500

    Add test for basic id generator

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,13 +1,20 @@
-import { validate } from "./utils";
+import { validate, generateID } from "./utils";
 
 describe("validate strings", () => {
-    it("throws on empty string", () => {
-        let theEmptyString = "";
-        let errorMessage = "The string may not be empty";
+  it("throws on empty string", () => {
+    let theEmptyString = "";
+    let errorMessage = "The string may not be empty";
 
-        expect(() => {
-            validate.string.isNotEmpty(theEmptyString, errorMessage);
-        }).toThrow(errorMessage)
-    })
+    expect(() => {
+      validate.string.isNotEmpty(theEmptyString, errorMessage);
+    }).toThrow(errorMessage);
+  });
+});
 
-})
+describe("Generate ID", () => {
+  it("creates a string", () => {
+    const id = generateID();
+
+    expect(typeof id === "string").toBe(true);
+  });
+});
