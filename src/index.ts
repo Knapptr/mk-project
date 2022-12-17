6e8fdb484a2701b40245f9378ea525bf14a8701d
Tyler Knapp <knapptr@gmail.com>
2022-12-17 07:58:13 -0500

    Scaffolds user and task routers

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import express from 'express';
 const PORT = process.env.PORT || 3000;
 const app = express();
+import router from "./Routers/index";
 
+// Entryway Middlewares
+app.use(express.json)
+
+// Routers
+app.use("/users", router.userRouter);
+app.use("/tasks", router.taskRouter);
+
+// Run Server
 app.listen(PORT, () => {
     console.log(`Server started. Listening on port ${PORT}`);
 })
