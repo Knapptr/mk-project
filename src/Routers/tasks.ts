0eb288088b654a1397baf8c8a1d996e1c4d9a876
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2022-12-20 11:17:10 -0500

    Add comments

--- src/Routers/tasks.ts
+++ src/Routers/tasks.ts
@@ -3,8 +3,10 @@ import tasks_controller from "../Controllers/tasks";
 
 const router = express.Router();
 
+// View all tasks
 router.get("/", tasks_controller.viewAll);
 
+// View specific task
 router.get("/:id", tasks_controller.viewOne);
 
 export default router;
